7a938d769e11c6bae8d829d0142a2b2981fee03b
Сидоров Андрей Алексеевич <asidorov@elveesneotek.internal>
2018-06-22 11:27:51 +0300

    On branch master: add output new and deleted lines.

--- check_file_update/check_file_update.cpp
+++ check_file_update/check_file_update.cpp
@@ -9,12 +9,13 @@
 #include <csignal>
 #include <iostream>
 #include <functional>
+#include <fstream>
+#include <unordered_map>
 #include <thread>
 
 #include <boost/optional.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
-
 
 boost::filesystem::path GetInputParams(int argc, const char* argv[])
 {
@@ -25,7 +26,7 @@ boost::filesystem::path GetInputParams(int argc, const char* argv[])
 	{
 		po::options_description cmd_params_description;
 		cmd_params_description.add_options()
-			("src_file", po::value<std::string>(), "type here input file name");
+			("src_file", po::value<std::string>()->required(), "type here input file name");
 
 		po::variables_map input_params;
 		po::store(po::parse_command_line(argc, argv, cmd_params_description), input_params);
@@ -48,7 +49,9 @@ boost::filesystem::path GetInputParams(int argc, const char* argv[])
 	return file_path;
 }
 
+// exit handler
 std::function<void(int)> exit_hanlder;
+
 void signal_hanlder(int signal)
 {
 	exit_hanlder(signal);
@@ -58,8 +61,10 @@ int main(int argc, const char* argv[])
 {
 	const auto file_path = GetInputParams(argc, argv);
 
+	if (file_path.empty())
+		return -1;
 
-	boost::optional<time_t> last_file_update_time {};
+	boost::optional<time_t> last_file_time_update {};
 
 	std::atomic_bool exit_condition = false;
 
@@ -72,9 +77,9 @@ int main(int argc, const char* argv[])
 	// set signal ctrl+c handler
 	signal(SIGINT, signal_hanlder);
 
-
-	std::thread check_file_update([&file_path, &last_file_update_time, &exit_condition]()
+	std::thread check_file_update([&file_path, &last_file_time_update, &exit_condition]()
 	{
+		std::unordered_multimap <size_t, std::string> file_map;
 		while (!exit_condition)
 		{
 			try
@@ -83,25 +88,55 @@ int main(int argc, const char* argv[])
 				auto status = fs::status(file_path);
 				if (fs::exists(status) && fs::is_regular_file(status))
 				{
-					const auto t = fs::last_write_time(file_path);
+					const auto file_time = fs::last_write_time(file_path);
 
-					if (last_file_update_time.is_initialized())
+					if (last_file_time_update.is_initialized())
 					{
-						const auto time_diff = t - last_file_update_time.get();
+						const auto time_diff = file_time - last_file_time_update.get();
 
 						if (time_diff > 0)
 						{
-							last_file_update_time = t;
-							std::cout << time_diff << "\r\n";
+							// update file time
+							last_file_time_update = file_time;
 							// get file diff
+							std::ifstream my_file(file_path.c_str(), std::ios::in);
+							std::string current_line;
+							decltype(file_map) updated_file_map;
+							while (getline(my_file, current_line))
+							{
+								auto line_hash = std::hash<std::string>()(current_line);
+
+								const auto same_line = file_map.find(line_hash);
+								if (same_line != file_map.end())
+								{
+									file_map.erase(same_line);
+								}
+								else
+								{
+									std::cout << "+\t" << current_line << "\r\n";
+								}
+								updated_file_map.emplace(line_hash, std::move(current_line));
+							}
+
+							for (const auto& line : file_map)
+							{
+								std::cout << "-\t" << line.second << "\r\n";
+							}
+
+							file_map.swap(updated_file_map);
 						}
 					}
 					else
 					{
-						last_file_update_time = t;
+						last_file_time_update = file_time;
 						//  only read file
+						std::ifstream my_file(file_path.c_str(), std::ios::in);
+						std::string current_line;
+						while (getline(my_file, current_line))
+						{
+							file_map.emplace(std::hash<std::string>()(current_line), std::move(current_line));
+						}
 					}
-
 				}
 			}
 			catch (const std::exception& e)
@@ -117,8 +152,6 @@ int main(int argc, const char* argv[])
 
 	if (check_file_update.joinable())
 		check_file_update.join();
-
-	std::cout << file_path << std::endl;
 
     return 0;
 }
